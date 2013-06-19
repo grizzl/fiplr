47442cfa76a82dcaa05c67be1f2c1ba0dd9a4c51
d11wtq <chris@w3style.co.uk>
2013-06-19 10:22:57 +1000

    Fix dependency declaration, missing symbol

--- fiplr-pkg.el
+++ fiplr-pkg.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author:   Chris Corbyn <chris@w3style.co.uk>
 ;; URL:      https://github.com/d11wtq/fiplr
-;; Version:  0.2.0
+;; Version:  0.2.1
 ;; Keywords: convenience, usability, project
 
 ;; This file is NOT part of GNU Emacs.
@@ -13,6 +13,6 @@
 
 ;; Licensed under the same terms as Emacs.
 
-(define-package "fiplr" "0.2.0"
+(define-package "fiplr" "0.2.1"
   "Fuzzy Search for Files in Projects"
-  '(("grizzl" "0.1.0")))
+  '((grizzl "0.1.0")))
